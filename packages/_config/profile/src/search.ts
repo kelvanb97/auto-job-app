f2d0c1a55f82037a6e46684d75e89294995a80c2
kelvanb97 <kelvanb97@gmail.com>
2026-03-19 01:48:04 -0700

    feat: create _config directory to store non-person-agnostic files

---
+++ packages/_config/profile/src/search.ts
@@ -0,0 +1,16 @@
+export const SEARCH_CONFIG = {
+	titles: [
+		"senior software engineer",
+		"senior frontend engineer",
+		"senior full-stack engineer",
+		"senior fullstack engineer",
+		"senior react engineer",
+		"senior typescript engineer",
+		"senior next.js engineer",
+		"senior web engineer",
+	],
+	remote: true,
+	fullTimeOnly: true,
+	freshnessdays: 3,
+	maxPagesPerQuery: 5,
+} as const
