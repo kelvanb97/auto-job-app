d88afbde796a67b081b2833198542de6e5d1e976
kelvanb97 <kelvanb97@gmail.com>
2026-03-30 00:14:17 -0700

    chore: remove packages/_core/supabase

--- packages/_core/supabase/src/admin.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-import { config } from "./config"
-
-export const supabaseAdminClient = <Database>() =>
-	createClient<Database>(config().supabase.url, config().supabase.secretKey)
